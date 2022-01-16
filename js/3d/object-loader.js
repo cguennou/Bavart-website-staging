deb2ccf46ad1f8a7004ce617bc36ed1a5def4d10
Chloé <c.guennou@gmail.com>
2022-01-16 21:37:51 +0100

    update version

--- js/3d/object-loader.js
+++ js/3d/object-loader.js
@@ -63,7 +63,7 @@
         // Load a glTF resource
         loader.load(
 	        // resource URL
-	        '3d/v4_buste02_idlle002.glb',
+	        './3d/Buste.gltf',
 	        // called when the resource is loaded
 	        function ( gltf ) {
 
