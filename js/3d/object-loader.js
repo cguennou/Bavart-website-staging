9dc13a62ffb536c7d07ca221f799fc6f41ec730a
ChloeG <c.guennou@gmail.com>
2022-01-30 15:22:15 +0100

    [test] upload new 3D

--- js/3d/object-loader.js
+++ js/3d/object-loader.js
@@ -49,21 +49,12 @@
         light4.position.set(500, 0, 10);
         scene.add( light4 );
 
-        
-        // objects
-        //loader = new THREE.JSONLoader();
         var loader = new THREE.GLTFLoader();
-        /*loader.load( '3d/deer.js', function ( geometry ) {
-            mesh = new THREE.Mesh( geometry, new THREE.MeshPhongMaterial( { color: 0xffffff, overdraw: 0.5 } ) );
-            mesh.position.y = 20;
-            mesh.scale.set(2.9,2.9,2.9);
-            scene.add( mesh );
-        } );*/
 
         // Load a glTF resource
         loader.load(
 	        // resource URL
-	        '3d/head_of_apollo/scene.gltf',
+	        '3d/cube_cascade/scene.gltf',
 	        // called when the resource is loaded
 	        function ( gltf ) {
 
